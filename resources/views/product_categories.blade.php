e4b114ae8ab00449554d21d9ae02c68ded2c26c4
Bailey van Antwerpen <99041848@mydavinci.nl>
2018-04-20 15:10:19 +0200

    update

--- resources/views/product_categories.blade.php
+++ resources/views/product_categories.blade.php
@@ -19,9 +19,9 @@
                             </div>
                             <div class="col-md-6">
                                 <ul>
-                                    <li><a href="{{ url('/products') }}">Category 1</a></li>
-                                    <li><a href="{{ url('/products') }}">Category 2</a></li>
-                                    <li><a href="{{ url('/products') }}">Category 3</a></li>
+                                    <li><a href="{{ url('/products') }}">Category 4</a></li>
+                                    <li><a href="{{ url('/products') }}">Category 5</a></li>
+                                    <li><a href="{{ url('/products') }}">Category 6</a></li>
                                 </ul>
                             </div>
                         </div>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -19,9 +19,9 @@
                             </div>
                             <div class="col-md-6">
                                 <ul>
-                                    <li><a href="{{ url('/product') }}">Product 1</a></li>
-                                    <li><a href="{{ url('/product') }}">Product 2</a></li>
-                                    <li><a href="{{ url('/product') }}">Product 3</a></li>
+                                    <li><a href="{{ url('/product') }}">Product 4</a></li>
+                                    <li><a href="{{ url('/product') }}">Product 5</a></li>
+                                    <li><a href="{{ url('/product') }}">Product 6</a></li>
                                 </ul>
                             </div>
                         </div>
