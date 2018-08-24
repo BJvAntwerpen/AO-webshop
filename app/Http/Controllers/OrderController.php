ccc487f0a370617417860a37411fa71a897e8f1d
Bailey van Antwerpen <99041848@mydavinci.nl>
2018-08-24 15:03:12 +0200

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-//use App\OrderModel;
+use App\OrderModel;
 
 class OrderController extends Controller
 {
